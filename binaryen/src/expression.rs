00b78a4f5a158bcff40e67c67862e8a895bc1533
Sami Kukkonen <sami@samikukkonen.fi>
2022-06-23 21:11:44 +0300

    Add note about unsafe pointer-to-reference cast

--- binaryen/src/expression.rs
+++ binaryen/src/expression.rs
@@ -28,6 +28,8 @@ impl Block {
     pub fn new<'a>(module: &'a Module, name: &str, type_: Type) -> &'a mut Block {
         let name = CString::new(name).unwrap();
         unsafe {
+            // SAFETY: Block is repr(transparent) so *mut BinaryenExpression can be cast to *mut Block.
+            // The BinaryenExpression is arena allocated and lives as long as the Module
             NonNull::new(BinaryenBlock(module.as_ptr(), name.as_ptr(), null_mut(), 0, type_.to_raw()))
                 .unwrap()
                 .cast()
